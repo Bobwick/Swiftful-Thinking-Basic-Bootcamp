921effa5b8f6c41fd316dc57918a5b9af9c71077
Robert Wick <robtimothywick@gmail.com>
2025-02-08 10:12:33 -0500

    Completed Lesson 1 of Swiftful Thinking

---
+++ SwiftfulThinkingBasicBootcamp.playground/Pages/Swift Basics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+
+
+// This is a single- line comment
+
+/*
+ This is a multi-line comment
+ that goes to
+ multiple lines!
+ */
+
+//Naming Conventions
+
+// Camel Case - Correct!
+// The first word is lowercased and then the first character in every following word is uppercased.
+let firstGreeting = "Hello World!"
+let thisIsMyFirstGreeting = "Hello World!"
+
+//  wrong
+let thisismysecondgreeting = "Hello World!"
+
+//  Snaked Case - wrong
+let this_is_what_snake_case_looks_like = "Hello World!"
+
+//  Pascal Case - wrong
+let ThisIsWhatPascalCaseLooksLike = "Hello World!"
+
+//  Camel Case - right
+let thisIsWhatCamelCaseLooksLike = "Hello World!"
+
+
